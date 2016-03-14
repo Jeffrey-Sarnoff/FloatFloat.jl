f92cf25b48eae200499cf4d1bf1d6af90e3ab522
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-14 03:18:47 -0400

    Create FloatFloats.jl

---
+++ src/FloatFloats.jl
@@ -0,0 +1,12 @@
+module FloatFloats
+
+import Base: hash, string, 
+    show, showcompact, showall,
+    convert, promote_rule,
+    isequal, isless, 
+    (==),(!=),(<),(<=),(>=),(>),
+    (+),(-),(*),(/),(\),(%),(^)
+
+export FloatFloat, FF
+
+end # module
