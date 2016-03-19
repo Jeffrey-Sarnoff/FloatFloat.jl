7c2d3e12daac8064e14310a3c7da6c7eb93c5c98
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 16:14:06 -0400

    Update FF.jl

--- src/type/FF.jl
+++ src/type/FF.jl
@@ -7,6 +7,7 @@ typealias FF32  FF{Float16}
 typealias FF64  FF{Float32}
 typealias FF128 FF{Float64}
 
-const hash_fflo = (UInt === UInt64) ? 0x086540d7a5325bc3 : 0x5acda43c
-const hash_0_fflo = hash(zero(UInt), hash_fflo)
-hash{T<:Real}(z::FF{T}, h::UInt) = hash(z.hi, h $ hash(z.lo, hash_fflow) $ hash_0_fflo)
+const hash_ff_lo = (UInt === UInt64) ? 0x086540d7a5325bc3 : 0x5acda43c
+const hash_0_ff_lo = hash(zero(UInt), hash_ff_lo)
+hash{T<:Real}(z::FF{T}, h::UInt) = 
+    hash(z.hi, (h $ hash(z.lo, hash_ff_lo) $ hash_0_ff_lo))
