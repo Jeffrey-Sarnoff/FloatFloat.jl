113306b8fd75f1e5dd3ce706b7b6e6ee677be857
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 06:51:22 -0400

    Update add.jl

--- src/arith/add.jl
+++ src/arith/add.jl
@@ -1,3 +1,21 @@
-(+){T<:Real}(a::FloatFloat{T}, b::FloatFloat{T})
+function (+){T<:Real}(a::FloatFloat{T}, b::FloatFloat{T{)
+    s1, s2 = eftAdd(a.hi,b.hi)
+    t1, t2 = eftAdd(a.lo,b.lo)
+    s2 += t1
+    s1, s2 = eftAddGTE(s1,s2)
+    s2 += t2
+    s1, s2 = eftAddGTE(s1,s2)
+    FloatFloat(s1,s2)
 end
 
+
+function (+){T<:Real}(a::FloatFloat{T},b::T)
+    s1, s2 = eftAdd(a.hi,b)
+    s2 += a.lo
+    s1, s2 = eftAddGTE(s1,s2)
+    FloatFloat(s1,s2)
+end
+
+(+){T<:Real}(a::T, b::FloatFloat{T}) = (+)(b,a)
+(+){T<:Real}(a::FloatFloat{T}, b::Signed) = (+)(a,convert(T,b))
+(+){T<:Real}(a::Signed, b::FloatFloat{T}) = (+)(b,a)
