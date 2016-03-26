080fbfd6306077d3d7a4b0ceba53ecdec604dbb7
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 06:33:52 -0400

    Update FF.jl

--- src/type/FF.jl
+++ src/type/FF.jl
@@ -1,11 +1,9 @@
-immutable FloatFloat{T<:StdFloat) <: Real
-    hi::T
-    lo::T
-end
-
-typealias FF32  FF{Float16}
-typealias FF64  FF{Float32}
-typealias FF128 FF{Float64}
+FF64{T1<:Real, T2<:Real}(hi::T1, lo::T2) = FF(Float64(hi),Float64(lo))
+FF32{T1<:Real, T2<:Real}(hi::T1, lo::T2) = FF(Float32(hi),Float32(lo))
+FF16{T1<:Real, T2<:Real}(hi::T1, lo::T2) = FF(Float16(hi),Float16(lo))
+FF64{T<:Real}(hi::T) = FloatFloat{Float64}(Float64(hi))
+FF32{T<:Real}(hi::T) = FloatFloat{Float32}(Float32(hi))
+FF16{T<:Real}(hi::T) = FloatFloat{Float16}(Float16(hi))
 
 const hash_ff_lo = (UInt === UInt64) ? 0x086540d7a5325bc3 : 0x5acda43c
 const hash_0_ff_lo = hash(zero(UInt), hash_ff_lo)
