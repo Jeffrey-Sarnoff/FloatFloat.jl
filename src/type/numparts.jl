8dc51a22313e78f8b22a3a95eab8d6b2929f587f
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 06:41:46 -0400

    Update numparts.jl

--- src/type/numparts.jl
+++ src/type/numparts.jl
@@ -1,33 +1,36 @@
-sign{T<:Real}(a::FF{T}) = sign(a.hi)
-signbit{T<:Real}(a::FF{T}) = signbit(a.hi)
+sign{T<:Real}(a::FloatFloat{T}) = sign(a.hi)
+signbit{T<:Real}(a::FloatFloat{T}) = signbit(a.hi)
 
-(-){T<:Real}(a::FF{T}) = FF(-a.hi,-a.lo)
-abs{T<:Real}(a::FF{T}) = (a.hi >= zero(T) ? a : -a)
+(-){T<:Real}(a::FloatFloat{T}) = FF(-a.hi,-a.lo)
+abs{T<:Real}(a::FloatFloat{T}) = (a.hi >= zero(T) ? a : -a)
 
-@inline flipsign{T<:Real}(a::FF{T},b::FF{T}) = FF(flipsign(a.hi,b.hi), flipsign(a.lo,b.hi))
-@inline flipsign{T<:Real}(a::FF{T},b::Float64) = FF(flipsign(a.hi,b), flipsign(a.lo,b))
-@inline flipsign{T<:Real}(a::FF{T},b::Integer) = FF(flipsign(a.hi,b), flipsign(a.lo,b))
+@inline flipsign{T<:Real}(a::FloatFloat{T},b::FloatFloat{T}) = 
+   FloatFloat(flipsign(a.hi,b.hi), flipsign(a.lo,b.hi))
+@inline flipsign{T<:Real}(a::FloatFloat{T},b::Float64) = 
+   FloatFloat(flipsign(a.hi,b), flipsign(a.lo,b))
+@inline flipsign{T<:Real}(a::FloatFloat{T},b::Integer) = 
+   FloatFloat(flipsign(a.hi,b), flipsign(a.lo,b))
 
-@inline function copysign{T<:Real}(a::FF{T},b::Float64)
+@inline function copysign{T<:Real}(a::FloatFloat{T},b::Float64)
     if (b < zero(Float64))
         a.hi < zero(Float64) ? a : -a
     else
         a.hi < zero(Float64) ? -a : a
     end
 end
-copysign{T<:Real}(a::FF{T},b::Integer) = copysign(a,convert(Float64,b))
-copysign{T<:Real}(a::FF{T},b::FF{T}) = copysign(a,b.hi)
+copysign{T<:Real}(a::FloatFloat{T},b::Integer) = copysign(a,convert(Float64,b))
+copysign{T<:Real}(a::FloatFloat{T},b::FloatFloat{T}) = copysign(a,b.hi)
 
 
-function frexp{T<:Real}(a::FF{T})
+function frexp{T<:Real}(a::FloatFloat{T})
     frhi, xphi = frexp(a.hi)
     frlo, xplo = frexp(a.lo)
     FF(frhi, ldexp(frlo,xplo-xphi)), xphi
 end
 
-function ldexp{T<:Real}(a::FF{T},xp::Int)
+function ldexp{T<:Real}(a::FloatFloat{T},xp::Int)
     FF(ldexp(a.hi,xp),ldexp(a.lo,xp))
 end
-ldexp{T<:Real,I<:Integer}(fx::Tuple{FF{T},I}) = ldexp(fx...)
+ldexp{T<:Real,I<:Integer}(fx::Tuple{FloatFloat{T},I}) = ldexp(fx...)
 
 
