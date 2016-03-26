d805867627f6d132df0d6fc03fea87817ea35f8a
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-26 08:55:18 -0400

    Update FloatFloats.jl

--- src/FloatFloats.jl
+++ src/FloatFloats.jl
@@ -17,7 +17,7 @@ export FloatFloat, FF, FF32, FF64, FF128,
     iszero, isnonzero, ispos, isneg, isnonneg,
     hash,
     NaNFF, InfFF,
-    mulby2, divby2, recip
+    mulby2, divby2, recip, sqr
 
 using ErrorfreeArithmetic
 # using AccurateArithmetic
