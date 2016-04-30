d35fb8bf332a5f06084aa114149882f52a21e88d
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-30 02:19:44 -0400

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -1,43 +1,43 @@
-function isless{T<:StdFloat}(a::FloatFloat{T}, b::FloatFloat{T})
+function isless{T<:SysFloat}(a::FloatFloat{T}, b::FloatFloat{T})
     (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
 end
-function isless{T<:StdFloat}(a::FloatFloat{T}, b::Float64)
+function isless{T<:SysFloat}(a::FloatFloat{T}, b::Float64)
     (a.hi < b) || (a.hi==b && a.lo<zero(Float64))
 end
-function isless{T<:StdFloat}(a::Float64, b::FloatFloat{T})
+function isless{T<:SysFloat}(a::Float64, b::FloatFloat{T})
     (a < b.hi) || (a==b.hi && b.lo<zero(Float64))
 end
-function isless{T<:StdFloat}(a::Float32, b::FloatFloat{T})
+function isless{T<:SysFloat}(a::Float32, b::FloatFloat{T})
     (a < b.hi) || (a==b.hi && b.lo<zero(Float32))
 end
-function isless{T<:StdFloat}(a::FloatFloat{T}, b::Float32)
+function isless{T<:SysFloat}(a::FloatFloat{T}, b::Float32)
     (a.hi < b) || (a.hi==b && a.lo<zero(Float32))
 end
 
-function isequal{T<:StdFloat}(a::FloatFloat{T}, b::FloatFloat{T})
+function isequal{T<:SysFloat}(a::FloatFloat{T}, b::FloatFloat{T})
     (a.hi == b.hi) && (a.lo == b.lo)
 end
-function isequal{T<:StdFloat}(a::FloatFloat{T}, b::Float64)
+function isequal{T<:SysFloat}(a::FloatFloat{T}, b::Float64)
     (a.hi == b) && (a.lo == zero(Float64))
 end
-function isequal{T<:StdFloat}(a::Float64, b::FloatFloat{T})
+function isequal{T<:SysFloat}(a::Float64, b::FloatFloat{T})
     (a == b.hi) && (b.lo == zero(Float64))
 end
-function isequal{T<:StdFloat}(a::FloatFloat{T}, b::Float32)
+function isequal{T<:SysFloat}(a::FloatFloat{T}, b::Float32)
     (a.hi == b) && (a.lo == zero(Float32))
 end
-function isequal{T<:StdFloat}(a::Float32, b::FloatFloat{T})
+function isequal{T<:SysFloat}(a::Float32, b::FloatFloat{T})
     (a == b.hi) && (b.lo == zero(Float32))
 end
 
-@inline (==){T<:StdFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi == b.hi) && (a.lo == b.lo)
-@inline (< ){T<:StdFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
-@inline (<=){T<:StdFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<=b.lo)
-@inline (> ){T<:StdFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>b.lo)
-@inline (>=){T<:StdFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>=b.lo)
+@inline (==){T<:SysFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi == b.hi) && (a.lo == b.lo)
+@inline (< ){T<:SysFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
+@inline (<=){T<:SysFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<=b.lo)
+@inline (> ){T<:SysFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>b.lo)
+@inline (>=){T<:SysFloat}(a::FloatFloat{T},b::FloatFloat{T}) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>=b.lo)
 
-min{T<:StdFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (a<b) ? a : b
-max{T<:StdFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (b<a) ? a : b
-minmax{T<:StdFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (a<b) ? (a,b) : (b,a)
-maxmin{T<:StdFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (b<a) ? (a,b) : (b,a)
+min{T<:SysFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (a<b) ? a : b
+max{T<:SysFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (b<a) ? a : b
+minmax{T<:SysFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (a<b) ? (a,b) : (b,a)
+maxmin{T<:SysFloat}(a::FloatFloat{T}, b::FloatFloat{T}) = (b<a) ? (a,b) : (b,a)
 
