b50b02bced8f162ca7f22be0f37a415c54e9302d
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-30 06:25:45 -0400

    Update exp.jl

--- src/math/exp.jl
+++ src/math/exp.jl
@@ -1074,22 +1074,3 @@ function exp(x::FloatFloat{Float64})
     rest   = rest * frac
     isneg ? inv(rest) : rest
 end    
-
-
-function expA(x::FloatFloat{Float64})
-    isneg = signbit(x)
-    x = abs(x)
-    fracpart, intpart = modf(x)
-    fracpart512 = fracpart * 512
-    n512ths = trunc(fracpart512)
-    fracpart = (fracpart512 - n512ths)/512
-    ths512 = exp_over512[ trunc(Int,n512ths)+1 ]
-    frac   = polyval(exp0_1o512_poly, fracpart)
-    rest   = exp_0to512[ 1+trunc(Int,intpart) ]
-    x = (rest * frac) * ths512
-    if isneg
-      x = inv(x)
-    end
-    x
-end    
-
