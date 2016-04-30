fd488a22ce68fe1802ad8f7299ef228c3f9762ca
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-30 02:23:14 -0400

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -1,19 +1,19 @@
-function string{T}(x::FloatFloat{T})
+function string{T<:SysFloat}(x::FloatFloat{T})
     string("FF(",x.hi,", ",x.lo,")")
 end
 
-function stringcompact{T}(x::FloatFloat{T})
+function stringcompact{T<:SysFloat}(x::FloatFloat{T})
     string("FF(",Float32(x.hi),", ",Float32(x.lo),")")
 end
 
-function show{T}(io::IO, x::FloatFloat{T})
+function show{T<:SysFloat}(io::IO, x::FloatFloat{T})
     print(io, string(x))
 end
 
-function showcompact{T}(io::IO, x::FloatFloat{T})
+function showcompact{T<:SysFloat}(io::IO, x::FloatFloat{T})
     print(io, stringcompact(x))
 end
 
-function showall{T}(io::IO, x::FloatFloat{T})
+function showall{T<:SysFloat}(io::IO, x::FloatFloat{T})
     print(io, string(x))
 end
