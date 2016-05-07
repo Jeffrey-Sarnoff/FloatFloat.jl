3e62ed181baf4ed992141f2b1c428ffe18728a85
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-05-07 06:33:38 -0400

    Update exp.jl

--- src/math/exp.jl
+++ src/math/exp.jl
@@ -718,7 +718,7 @@ function exp(x::FloatFloat{Float64})
       fpart = exp0to1(fpart)
       if ipart != 1
           exp0to512[ipart+1] * fpart
-      else                                          # add in triple-double(exp(1)) least sig * fpart.hi
+      else      # double-double(exp(1)) * fpart, add in triple-double(exp(1)) least sig part * fpart.hi
           muladd(FF(2.718281828459045, 1.4456468917292502e-16),fpart, -2.1277171080381768e-33*fpart.hi) 
       end
    else
